cb6f797e3fafafa64a431acaa0fdd448c73cead4
kc-prashant <weebprashant@gmail.com>
2026-03-14 21:48:41 +0545

    h

--- write_file.py
+++ write_file.py
@@ -1,2 +1,9 @@
 with open("text.txt" , "w") as file:
     file.write("this is new line written in the file removing old one")
+with open("text.txt" , "a") as file:
+    file.write (" this is line added to the previous file \n")
+    file.write(" this is another file")
+
+with open ("text.txt", "r") as file:
+    content = file.read()
+    print (content)
