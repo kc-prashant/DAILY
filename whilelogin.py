5cd499f7864761b6e6dd7321e0f38f3a29d92c30
kc-prashant <weebprashant@gmail.com>
2026-03-17 07:50:44 +0545

    while loop

---
+++ whilelogin.py
@@ -0,0 +1,11 @@
+while True:
+    print("Who are you ?")
+    name = input (">")
+    if name != "prashant":
+        continue
+    print("hello prashant , please enter your password")
+    password = input("> ")
+    if password == "apple":
+        break
+
+print( "welcome to dashboard!!!")
