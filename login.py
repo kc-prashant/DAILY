ddeac2f55efc752d4e32c4fc256f1e66244bff76
kc-prashant <weebprashant@gmail.com>
2026-03-17 07:26:25 +0545

    simple login login

---
+++ login.py
@@ -0,0 +1,12 @@
+name = "prashant"
+password = "apple"
+
+name=input("enter your name > ")
+
+if name == "prashant":
+    print("hello prashant")
+    password = input("enter your password > ")
+    if password == "apple":
+        print ("login successfully")
+    else:
+        print("incorrect password")
