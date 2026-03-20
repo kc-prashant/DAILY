b52dd71b22181f94873f536f91fcebe3d2695c07
kc-prashant <weebprashant@gmail.com>
2026-03-20 22:30:50 +0545

    coin

---
+++ coin.py
@@ -0,0 +1,16 @@
+import random
+head =0
+tail =0
+for i in range(100):  
+    if random.randint(0, 1) == 0:
+        
+        print('H', end=' ')
+        head =+1
+    else:
+        print('T', end=' ')
+        tail =+1
+        
+
+print()  
+print (f"total heads are {head}")
+print (f"total tails are {tail}")
