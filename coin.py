9813dd283be916f15bc30c4fe63bfea71fd564b7
kc-prashant <weebprashant@gmail.com>
2026-03-21 22:05:05 +0545

    .

--- coin.py
+++ coin.py
@@ -5,10 +5,10 @@ for i in range(100):
     if random.randint(0, 1) == 0:
         
         print('H', end=' ')
-        head =+1
+        head +=1
     else:
         print('T', end=' ')
-        tail =+1
+        tail +=1
         
 
 print()  
