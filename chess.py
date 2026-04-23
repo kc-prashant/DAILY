09353b7d2b1840a8f4a7d17271a2c00068ccacc7
kc-prashant <weebprashant@gmail.com>
2026-04-23 16:37:03 +0545

    /

--- chess.py
+++ chess.py
@@ -1,11 +1,15 @@
-import sys,copy
+import sys, copy
 
-STARTING_PIECES = { 'a8':'bR','b8': 'bN', 'c8': 'bB', 'd8': 'bQ',
-'e8': 'bK', 'f8': 'bB', 'g8': 'bN', 'h8': 'bR', 'a7': 'bP', 'b7': 'bP',
-'c7': 'bP', 'd7': 'bP', 'e7': 'bP', 'f7': 'bP', 'g7': 'bP', 'h7': 'bP',
-'a1': 'wR', 'b1': 'wN', 'c1': 'wB', 'd1': 'wQ', 'e1': 'wK', 'f1': 'wB',
-'g1': 'wN', 'h1': 'wR', 'a2': 'wP', 'b2': 'wP', 'c2': 'wP', 'd2': 'wP',
-'e2': 'wP', 'f2': 'wP', 'g2': 'wP', 'h2': 'wP'}
+STARTING_PIECES = {
+    'a8':'bR','b8': 'bN', 'c8': 'bB', 'd8': 'bQ',
+    'e8': 'bK', 'f8': 'bB', 'g8': 'bN', 'h8': 'bR',
+    'a7': 'bP', 'b7': 'bP','c7': 'bP', 'd7': 'bP',
+    'e7': 'bP', 'f7': 'bP', 'g7': 'bP', 'h7': 'bP',
+    'a1': 'wR', 'b1': 'wN', 'c1': 'wB', 'd1': 'wQ',
+    'e1': 'wK', 'f1': 'wB','g1': 'wN', 'h1': 'wR',
+    'a2': 'wP', 'b2': 'wP', 'c2': 'wP', 'd2': 'wP',
+    'e2': 'wP', 'f2': 'wP', 'g2': 'wP', 'h2': 'wP'
+}
 
 BOARD_TEMPLATE = """ 
    a    b    c    d    e    f    g    h
@@ -34,39 +38,62 @@ BOARD_TEMPLATE = """
   |    ||||||    ||||||    ||||||    ||||||
 1 | {} ||{}|| {} ||{}|| {} ||{}|| {} ||{}||
   |____||||||____||||||____||||||____||||||
-
 """
+
 WHITE_SQUARE = '||'
 BLACK_SQUARE = '  '
 
 def print_chessboard(board):
     squares = []
     is_white_square = True
+
     for y in '87654321':
         for x in 'abcdefgh':
-            print(x,y, is_white_square)
-
-            if x + y in board.keys():
+            if x + y in board:
                 squares.append(board[x + y])
             else:
-                if is_white_square:
-                    squares.append(WHITE_SQUARE)
-                else:
-                    squares.append(BLACK_SQUARE)
-            
+                squares.append(WHITE_SQUARE if is_white_square else BLACK_SQUARE)
+
             is_white_square = not is_white_square
         is_white_square = not is_white_square
 
     print(BOARD_TEMPLATE.format(*squares))
 
-    if x + y in board.keys():
-                squares.append(board[x + y])
-            else:
-                if is_white_square:
-                    squares.append(WHITE_SQUARE)
-                else:
-                    squares.append(BLACK_SQUARE)
-            is_white_square = not is_white_square
-        is_white_square = not is_white_square
 
-    print(BOARD_TEMPLATE.format(*squares))
+print('Interactive Chessboard')
+print('Commands: move e2 e4 | remove e2 | set e2 wP | reset | clear | fill wP | quit')
+
+main_board = copy.copy(STARTING_PIECES)
+
+while True:
+    print_chessboard(main_board)
+    response = input('> ').split()
+
+    if not response:
+        continue
+
+    if response[0] == 'move':
+        if response[1] in main_board:
+            main_board[response[2]] = main_board[response[1]]
+            del main_board[response[1]]
+
+    elif response[0] == 'remove':
+        if response[1] in main_board:
+            del main_board[response[1]]
+
+    elif response[0] == 'set':
+        main_board[response[1]] = response[2]
+
+    elif response[0] == 'reset':
+        main_board = copy.copy(STARTING_PIECES)
+
+    elif response[0] == 'clear':
+        main_board = {}
+
+    elif response[0] == 'fill':
+        for y in '87654321':
+            for x in 'abcdefgh':
+                main_board[x + y] = response[1]
+
+    elif response[0] == 'quit':
+        sys.exit()
